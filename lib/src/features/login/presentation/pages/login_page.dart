fb11ec0af58a8b0a31dddc6b5e4a94d2d56c9189
saralesan <saralesan@hotmail.com>
2024-01-13 22:42:22 -0500

    login bg and details

--- lib/src/features/login/presentation/pages/login_page.dart
+++ lib/src/features/login/presentation/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:bflow_client/src/core/extensions/build_context_extensions.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -6,76 +7,129 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: Row(
         children: [
-          const Text(
-            "Welcome back!",
-            style: TextStyle(fontSize: 20),
-          ),
-          const Text(
-            "Please enter your details",
-            style: TextStyle(color: Colors.grey),
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Text("User"),
-          ),
-          TextField(
-            decoration: InputDecoration(
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8),
-                borderSide: const BorderSide(color: Colors.grey, width: 1),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8),
-                borderSide: const BorderSide(color: Colors.grey, width: 1.5),
-              ),
-              //contentPadding: const EdgeInsets.all(10),
-              isDense: true,
-              filled: true,
-              fillColor: Colors.white,
-
-              hintText: "",
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Text("Password"),
-          ),
-          TextField(
-            decoration: InputDecoration(
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8),
-                borderSide: const BorderSide(color: Colors.grey, width: 1),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8),
-                borderSide: const BorderSide(color: Colors.grey, width: 1.5),
-              ),
-              //contentPadding: const EdgeInsets.all(10),
-              isDense: true,
-              filled: true,
-              fillColor: Colors.white,
-
-              hintText: "",
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 30, right: 20, left: 20),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                //padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 20), // THIS no hace nada
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.blue,
-                minimumSize: const Size.fromHeight(60), // THIs full with?
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
+          context.isDesktop ? Expanded(
+            flex: 1,
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 0,
+                  bottom: 0,
+                  right: 0,
+                  left: 0,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/img/background_login.png'),
+                        fit: BoxFit.cover
+                      )
+                    ),
+                  ),
                 ),
-              ),
-              onPressed: () {},
-              child: const Text(
-                'Log in',
+                Positioned(
+                  top: 0,
+                  child: Container(
+                    height: 300,
+                    width: 380,
+                    decoration: BoxDecoration(
+                      color: const Color(0x80000000).withOpacity(0.6),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Image.asset('assets/img/sh_logo_and_text.png'),
+                  ),
+                ),
+              ],
+            ),
+          ) : const SizedBox.shrink(),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: Container(
+                constraints: const BoxConstraints(maxWidth: 500),
+                margin: const EdgeInsets.symmetric(horizontal: 80),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Welcome back!",
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    const SizedBox(height: 35),
+                    const Text(
+                      "Please enter your details",
+                      style: TextStyle(fontSize: 16, color: Colors.grey),
+                    ),
+                    const SizedBox(height: 25),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 8),
+                      child: Text("User"),
+                    ),
+                    const SizedBox(height: 5),
+                    TextField(
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: const BorderSide(color: Colors.grey, width: 1),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: const BorderSide(color: Colors.grey, width: 1.5),
+                        ),
+                        //contentPadding: const EdgeInsets.all(10),
+                        isDense: true,
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: "",
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 8),
+                      child: Text("Password"),
+                    ),
+                    const SizedBox(height: 5),
+                    TextField(
+                      obscureText: true,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: const BorderSide(color: Colors.grey, width: 1),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: const BorderSide(color: Colors.grey, width: 1.5),
+                        ),
+                        //contentPadding: const EdgeInsets.all(10),
+                        isDense: true,
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: "",
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30, right: 20, left: 20),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.blue,
+                          minimumSize: const Size.fromHeight(60),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: const Text(
+                          'Log in',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
