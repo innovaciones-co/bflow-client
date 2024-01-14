11175c2645e509907cc80b57e5f014dd03183993
saralesan <saralesan@hotmail.com>
2024-01-14 01:15:17 -0500

    fixed horizontal scroll for job_item

--- lib/src/features/jobs/presentation/widgets/job_item_widget.dart
+++ lib/src/features/jobs/presentation/widgets/job_item_widget.dart
@@ -16,12 +16,72 @@ class JobItemWidget extends StatelessWidget {
       ), 
       height: 120, // THIS
       margin: const EdgeInsets.symmetric(vertical: 10),
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        padding: const EdgeInsets.only(left: 15, right: 8, top: 15, bottom: 15),
+      child: context.isMobile || context.isTablet || context.isSmallTablet ? _mobileLayout(context) : _desktopLayout(context),
+    );
+  }
+
+  Widget _mobileLayout(BuildContext context) {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      padding: const EdgeInsets.only(left: 15, right: 8, top: 15, bottom: 15),
+      children: [
+        _mobileCellJob(title: "Job Number", width: 110, child: const Text("SH2201")),
+        _mobileCellJob(title: "Address", width: 160, 
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text("#94 Pola st Dianella"),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.all(0)
+                ),
+                child: const Text("Open in Goole maps"),
+              ), // THIS ?link
+            ],
+          )
+        ),
+        _mobileCellJob(title: "Supervisor", width: 140, child: const Text("Alberto Federico")),
+        _mobileCellJob(title: "Job Stage", width: 110, child: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomChipWidget(label: "Slab down",),
+          ],
+        ),),
+        _mobileCellJob(title: "Days in construction", width: 160, child: const Text("90")),
+        _mobileCellJob(title: "Progress", width: 150, child: _progressBar(percentage: 0.7, width: 140),
+        ),
+        const SizedBox(width: 40),
+        Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 20), // THIS no hace nada
+                foregroundColor: context.primary,
+                backgroundColor: context.background,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5)
+                ),
+              ),
+              onPressed: () {},
+              child: const Text(
+                'View details',
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _desktopLayout(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 15, right: 8, top: 15, bottom: 15),
+      child: Row(
         children: [
-          _cellJob(title: "Job Number", width: 110, child: const Text("SH2201")),
-          _cellJob(title: "Address", width: 160, 
+          _cellJob(title: "Job Number", flex: 1, child: const Text("SH2201")),
+          _cellJob(title: "Address", flex: 2, 
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -36,70 +96,76 @@ class JobItemWidget extends StatelessWidget {
               ],
             )
           ),
-          _cellJob(title: "Supervisor", width: 140, child: const Text("Alberto Federico")),
-          _cellJob(title: "Job Stage", width: 110, child: const Column(
+          _cellJob(title: "Supervisor", flex: 2, child: const Text("Alberto Federico")),
+          _cellJob(title: "Job Stage", flex: 2, child: const Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               CustomChipWidget(label: "Slab down",),
             ],
           ),),
-          _cellJob(title: "Days in construction", width: 160, child: const Text("90")),
-          _cellJob(title: "Progress", width: 150, child: _progressBar(percentage: 0.7, width: 140),
-          ),
-          const SizedBox(width: 40,),
-          Column(
-            //mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 20), // THIS no hace nada
-                  foregroundColor: context.primary,
-                  backgroundColor: context.background,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5)
+          _cellJob(title: "Days in construction", flex: 2, child: const Text("90")),
+          _cellJob(title: "Progress", flex: 2, child: _progressBar(percentage: 0.7, width: 200)),
+          Expanded(
+            flex: 2,
+            child: Column(
+              //mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 20), // THIS no hace nada
+                    foregroundColor: context.primary,
+                    backgroundColor: context.background,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5)
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    'View details',
                   ),
                 ),
-                onPressed: () {},
-                child: const Text(
-                  'View details',
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
     );
   }
+}
 
-  Column _progressBar({required double percentage, required double width}) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          height: 10,
-          width: width,
-          margin: const EdgeInsets.only(top: 7),
-          decoration: BoxDecoration(
-            color: Colors.grey.shade400,
-            borderRadius: BorderRadius.circular(5),
-          ),
-          child: FractionallySizedBox(
-            widthFactor: percentage, // THIS porcentaje
-            alignment: Alignment.centerLeft,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.orange.shade700,
-                borderRadius: BorderRadius.circular(5),
+
+  Widget _progressBar({required double percentage, required double width}) {
+    return SizedBox(
+      width: width,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 10,
+            width: width,
+            margin: const EdgeInsets.only(top: 7),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade400,
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: FractionallySizedBox(
+              widthFactor: percentage, // THIS porcentaje
+              alignment: Alignment.centerLeft,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.orange.shade700,
+                  borderRadius: BorderRadius.circular(5),
+                ),
               ),
             ),
           ),
-        ),
-        Center(child: Text("${percentage*100}% complete",)),
-      ],
+          Center(child: Text("${percentage*100}% complete",)),
+        ],
+      ),
     );
   }
 
-  SizedBox _cellJob({required String title, required double width, required Widget child}) {
+  SizedBox _mobileCellJob({required String title, required double width, required Widget child}) {
     return SizedBox(
       width: width,
       child: Column(
@@ -115,4 +181,20 @@ class JobItemWidget extends StatelessWidget {
       ),
     );
   }
-}
+
+  Widget _cellJob({required String title, required int flex, required Widget child}) {
+    return Expanded(
+      flex: flex,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: const TextStyle(color: Colors.grey),
+          ),
+          const SizedBox(height: 10),
+          child,
+        ],
+      ),
+    );
+  }
