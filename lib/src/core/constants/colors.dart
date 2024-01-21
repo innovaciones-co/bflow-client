9e37837c33216e388fa76853b2e35a07a6c9a864
Sergio Torres <sdtorresl@unal.edu.co>
2024-01-20 19:52:57 -0500

    color theme

--- lib/src/core/constants/colors.dart
+++ lib/src/core/constants/colors.dart
@@ -3,10 +3,30 @@ library app_colors;
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static Color black = const Color(0xFF000000);
+  // Basic Colors
+  static Color black = const Color(0xFF202020);
   static Color white = const Color(0xFFFFFFFF);
+  static Color blue = const Color(0xff3483FA);
+  static Color lightBlue = const Color(0xffe3eefc);
+  static Color darkGrey = const Color(0xff737373);
+  static Color grey = const Color(0xffD9D9D9);
+  static Color lightGrey = const Color(0xffF5F5F5);
+
+  // Other Colors
   static Color purple = const Color(0xffab86ff);
   static Color lightPurple = const Color(0xfff0ecff);
   static Color green = const Color(0xff38BB9C);
   static Color lightGreen = const Color(0xffd9f2e0);
+  static Color orange = const Color(0xffFF7733);
+
+  // To do list Colors
+  static List<Color> todoColors = [
+    const Color(0xfFF0ECFF),
+    const Color(0xfffbdcf2),
+    const Color(0xffdde3fb),
+    const Color(0xffFBEADE),
+    const Color(0xffC0EDE3),
+  ];
+
+  // TODO: get randomColors
 }
