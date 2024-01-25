0c08706193334c5c52ef7888790dfca7505400e2
saralesan <saralesan@hotmail.com>
2024-01-24 23:18:28 -0500

    Basic job documents tabs

--- lib/src/features/jobs/presentation/widgets/job_files_widget.dart
+++ lib/src/features/jobs/presentation/widgets/job_files_widget.dart
@@ -5,6 +5,39 @@ class JobFilesWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Job files Widget");
+    return const Expanded(
+      //height: 100,
+      child: DefaultTabController(
+        length: 6,
+        child: Scaffold(
+          body: Column(
+            children: [
+              TabBar(
+                tabs: [
+                  Tab(text: ("Slab down")),
+                  Tab(text: ("Plate Height")),
+                  Tab(text: ("Roof Cover")),
+                  Tab(text: ("Lock UP")),
+                  Tab(text: ("Cabinets")),
+                  Tab(text: ("PCI")),
+                ],
+              ),
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    Text("Slab down"),
+                    Text("Plate Height"),
+                    Text("Roof Cover"),
+                    Text("Lock UP"),
+                    Text("Cabinets"),
+                    Text("PCI"),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
