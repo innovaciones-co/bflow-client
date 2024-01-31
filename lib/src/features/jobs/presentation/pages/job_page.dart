0437c5f21e307d8b5e3b03ab4c0adcab56cd8c06
saralesan <saralesan@hotmail.com>
2024-01-30 23:22:10 -0500

    Organize Job buttons

--- lib/src/features/jobs/presentation/pages/job_page.dart
+++ lib/src/features/jobs/presentation/pages/job_page.dart
@@ -3,7 +3,6 @@ import 'package:bflow_client/src/core/constants/colors.dart';
 import 'package:bflow_client/src/core/widgets/action_button_widget.dart';
 import 'package:bflow_client/src/core/widgets/failure_widget.dart';
 import 'package:bflow_client/src/core/widgets/page_container_widget.dart';
-import 'package:bflow_client/src/core/widgets/switch_widget.dart';
 import 'package:bflow_client/src/features/jobs/presentation/bloc/job_bloc.dart';
 import 'package:bflow_client/src/features/jobs/presentation/bloc/tasks_bloc.dart';
 import 'package:bflow_client/src/features/jobs/presentation/widgets/job_item_widget.dart';
@@ -79,6 +78,7 @@ class _JobPageState extends State<JobPage> {
         JobItemWidget(job: state.job),
         _jobViewSelection(),
         const FilterTasksWidget(),
+        const SizedBox(height: 5),
         _body
       ],
     );
@@ -102,9 +102,6 @@ class _JobPageState extends State<JobPage> {
             type: ButtonType.textButton,
             title: "View Calendar",
             icon: Icons.task_outlined),
-        const SwitchWidget(
-          title: "View calendar",
-        ),
       ],
     );
   }
@@ -124,36 +121,55 @@ class FilterTasksWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        const Text("Search"),
-        ActionButtonWidget(
-          onPressed: () {},
-          type: ButtonType.textButton,
-          title: "Filter",
-          icon: Icons.tune,
-          foregroundColor: AppColor.black,
+        Row(
+          children: [
+            const Text("Search"),
+            Container(
+              width: 1,
+              height: 20,
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              color: AppColor.grey,
+            ),
+            ActionButtonWidget(
+              onPressed: () {},
+              type: ButtonType.textButton,
+              title: "Filter",
+              icon: Icons.tune,
+              foregroundColor: AppColor.black,
+            ),
+            const Text("Water Meter call up"),
+          ],
         ),
-        const Text("Water Meter call up"),
-        ActionButtonWidget(
-            onPressed: () {},
-            type: ButtonType.textButton,
-            title: "Delete",
-            icon: Icons.delete_outline),
-        ActionButtonWidget(
-          onPressed: () {},
-          type: ButtonType.elevatedButton,
-          title: "Send task",
-          icon: Icons.mail_outline,
-          backgroundColor: AppColor.lightBlue,
-        ),
-        const SizedBox(width: 12),
-        ActionButtonWidget(
-          onPressed: () {},
-          type: ButtonType.elevatedButton,
-          title: "New Activity",
-          icon: Icons.add,
-          backgroundColor: AppColor.blue,
-          foregroundColor: AppColor.white,
+        Row(
+          children: [
+            ActionButtonWidget(
+              onPressed: () {},
+              type: ButtonType.textButton,
+              title: "Delete",
+              icon: Icons.delete_outline,
+              paddingHorizontal: 15,
+              paddingVertical: 18,
+            ),
+            const SizedBox(width: 12),
+            ActionButtonWidget(
+              onPressed: () {},
+              type: ButtonType.elevatedButton,
+              title: "Send task",
+              icon: Icons.mail_outline,
+              backgroundColor: AppColor.lightBlue,
+            ),
+            const SizedBox(width: 12),
+            ActionButtonWidget(
+              onPressed: () {},
+              type: ButtonType.elevatedButton,
+              title: "New Activity",
+              icon: Icons.add,
+              backgroundColor: AppColor.blue,
+              foregroundColor: AppColor.white,
+            ),
+          ],
         ),
       ],
     );
