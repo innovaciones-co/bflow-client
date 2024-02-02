41f747467a4b3e7f289a9c7de35c847615cb7df0
saralesan <saralesan@hotmail.com>
2024-02-01 22:03:09 -0500

    create downloadFileWidget

--- lib/src/features/jobs/presentation/widgets/job_files_widget.dart
+++ lib/src/features/jobs/presentation/widgets/job_files_widget.dart
@@ -1,4 +1,6 @@
 import 'package:bflow_client/src/core/constants/colors.dart';
+import 'package:bflow_client/src/core/widgets/action_button_widget.dart';
+import 'package:bflow_client/src/features/jobs/presentation/widgets/download_file_widget.dart';
 import 'package:flutter/material.dart';
 
 class JobFilesWidget extends StatelessWidget {
@@ -6,6 +8,36 @@ class JobFilesWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Files");
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text("Documents"),
+            ActionButtonWidget(
+              onPressed: () {},
+              type: ButtonType.elevatedButton,
+              title: "Attach",
+              icon: Icons.attach_file_outlined,
+              backgroundColor: AppColor.blue,
+              foregroundColor: AppColor.white,
+            )
+          ],
+        ),
+        const SizedBox(
+          width: double.infinity,
+          child: Wrap(
+            spacing: 10,
+            alignment: WrapAlignment.start,
+            children: [
+              DownloadFileWidget(),
+              DownloadFileWidget(),
+              DownloadFileWidget(),
+              DownloadFileWidget(),
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
