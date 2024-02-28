70cbc2766326cfce445f29dcec3152f738438aa3
saralesan <saralesan@hotmail.com>
2024-02-27 21:08:44 -0500

    Create DialogWidget

---
+++ lib/src/core/widgets/dialog_widget.dart
@@ -0,0 +1,47 @@
+import 'package:bflow_client/src/core/constants/colors.dart';
+import 'package:bflow_client/src/core/extensions/build_context_extensions.dart';
+import 'package:flutter/material.dart';
+
+class DialogWidget extends StatelessWidget {
+  const DialogWidget({
+    super.key,
+    this.title,
+    required this.children,
+  });
+
+  final String? title;
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+      child: Container(
+        constraints: BoxConstraints(
+          maxWidth:
+              context.isDesktop ? context.width * 0.3 : context.width * 0.8,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: AppColor.white,
+        ),
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            title != null
+                ? Text(
+                    title!,
+                    style: context.titleLarge
+                        ?.copyWith(fontWeight: FontWeight.bold),
+                  )
+                : const SizedBox(),
+            title != null ? const SizedBox(height: 15) : const SizedBox(),
+            ...children,
+          ],
+        ),
+      ),
+    );
+  }
+}
