6f75fe0d94ddf4a9d4cdf423cba7e3e9bdd7fe8a
saralesan <saralesan@hotmail.com>
2024-02-29 21:37:38 -0500

    Additional fields for WriteContactWidget

--- lib/src/features/jobs/presentation/widgets/write_contact_widget.dart
+++ lib/src/features/jobs/presentation/widgets/write_contact_widget.dart
@@ -1,7 +1,15 @@
 import 'package:bflow_client/src/core/constants/colors.dart';
 import 'package:bflow_client/src/core/widgets/action_button_widget.dart';
+import 'package:bflow_client/src/core/widgets/dropdown_widget.dart';
 import 'package:bflow_client/src/core/widgets/input_widget.dart';
 import 'package:flutter/material.dart';
+
+const List<String> list = [
+  'One',
+  'Two',
+  'Three',
+  'Four',
+]; // TODO: Delete when DropDown completed
 
 class WriteContactWidget extends StatelessWidget {
   const WriteContactWidget({super.key});
@@ -12,17 +20,43 @@ class WriteContactWidget extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        const InputWidget(label: "Name"),
+        const SizedBox(height: 20),
         const Row(
           children: [
-            Expanded(child: InputWidget(label: "Name")),
+            Expanded(child: InputWidget(label: "Email")),
             SizedBox(width: 15),
-            Expanded(child: InputWidget(label: "Last Name")),
+            Expanded(child: InputWidget(label: "Phone number")),
           ],
         ),
         const SizedBox(height: 20),
-        const InputWidget(label: "Email"),
-        const SizedBox(height: 20),
         const InputWidget(label: "Address"),
+        const SizedBox(height: 20),
+        DropdownWidget<String>(
+          label: "Type",
+          items: list,
+          getLabel: (s) => s,
+          onChanged: null,
+          initialValue: list.first,
+        ),
+        const SizedBox(height: 20),
+        const Row(
+          children: [
+            Expanded(child: InputWidget(label: "Account number")),
+            SizedBox(width: 15),
+            Expanded(child: InputWidget(label: "Holder name")),
+          ],
+        ),
+        const SizedBox(height: 20),
+        const Row(
+          children: [
+            Expanded(child: InputWidget(label: "Bank name")),
+            SizedBox(width: 15),
+            Expanded(child: InputWidget(label: "Tax number")),
+          ],
+        ),
+        const SizedBox(height: 20),
+        const InputWidget(label: "Details"),
         const SizedBox(height: 20),
         Row(
           mainAxisAlignment: MainAxisAlignment.end,
