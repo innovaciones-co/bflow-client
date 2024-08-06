36cd7354108f0c1434b6b3c14820fa8a66d1572c
saralesan <saralesan@hotmail.com>
2024-08-05 21:36:27 -0500

    (chore) Remove commented lines

--- lib/src/features/jobs/presentation/widgets/job_tasks_widget.dart
+++ lib/src/features/jobs/presentation/widgets/job_tasks_widget.dart
@@ -1,5 +1,4 @@
 import 'package:bflow_client/src/core/extensions/build_context_extensions.dart';
-import 'package:bflow_client/src/features/jobs/presentation/bloc/tasks/tasks_bloc.dart';
 import 'package:bflow_client/src/features/jobs/presentation/bloc/tasks_filter/tasks_filter_bloc.dart';
 import 'package:bflow_client/src/features/jobs/presentation/widgets/list_tasks_desktop_widget.dart';
 import 'package:bflow_client/src/features/jobs/presentation/widgets/list_tasks_mobile_widget.dart';
@@ -11,43 +10,24 @@ class JobTasksWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<TasksBloc, TasksState>(
-      listener: (context, state) {
-        /* if (state is TasksLoaded) {
-          if (!state.tasksUpdated) {
-            return;
-          }
-
-          HomeBloc homeBloc = context.read();
-          homeBloc.add(
-            const ShowFooterActionEvent(
-              actions: [
-                ElevatedButton(onPressed: null, child: Text("Save")),
-              ],
-              leading: Text("Update (10) selected tasks"),
-            ),
+    return BlocBuilder<TasksFilterBloc, TasksFilterState>(
+      builder: (context, state) {
+        if (state is TasksFilterLoading) {
+          return const Center(
+            child: CircularProgressIndicator(),
           );
-        } */
+        }
+
+        if (state is TasksFilterLoaded) {
+          var tasks = state.tasks;
+          var tabIndex = state.tabIndex;
+          return context.isMobile || context.isSmallTablet
+              ? ListTasksMobileWidget(tasks: tasks)
+              : ListTasksDesktopWidget(tasks: tasks, tabIndex: tabIndex);
+        }
+
+        return const SizedBox.shrink();
       },
-      child: BlocBuilder<TasksFilterBloc, TasksFilterState>(
-        builder: (context, state) {
-          if (state is TasksFilterLoading) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          if (state is TasksFilterLoaded) {
-            var tasks = state.tasks;
-            var tabIndex = state.tabIndex;
-            return context.isMobile || context.isSmallTablet
-                ? ListTasksMobileWidget(tasks: tasks)
-                : ListTasksDesktopWidget(tasks: tasks, tabIndex: tabIndex);
-          }
-
-          return const SizedBox.shrink();
-        },
-      ),
     );
   }
 }
