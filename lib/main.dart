3ec74f50b8ad39abe4e0826c5e59e5d7dda8ecb0
Sergio Torres <sdtorresl@unal.edu.co>
2024-08-25 21:41:50 -0500

    refactor: Remove unnecessary comments

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,6 @@ import 'bflow.dart';
 import 'src/core/config/config.dart';
 
 Future<void> main() async {
-  // Test commit
-  // Other
   await DependencyInjection.init();
   runApp(const BflowApp());
 }
