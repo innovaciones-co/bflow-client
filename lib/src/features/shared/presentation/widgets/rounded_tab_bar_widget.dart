303acacef1fad122812c416352b1baf138c1b9e1
saralesan <saralesan@hotmail.com>
2024-09-29 01:55:44 -0500

    style: Update roundedBar colors

--- lib/src/features/shared/presentation/widgets/rounded_tab_bar_widget.dart
+++ lib/src/features/shared/presentation/widgets/rounded_tab_bar_widget.dart
@@ -52,8 +52,9 @@ class _RoundedTabBarWidgetState extends State<RoundedTabBarWidget> {
     bool first = false,
     bool last = false,
   }) {
-    Color foregroundColor = isExpanded ? AppColor.lightBlue : AppColor.darkGrey;
-    Color backgroundColor = isExpanded ? AppColor.blue : AppColor.grey;
+    Color foregroundColor = isExpanded ? AppColor.blue : AppColor.darkGrey;
+    Color backgroundColor =
+        isExpanded ? AppColor.blue.withOpacity(0.1) : AppColor.lightGrey;
     return AnimatedContainer(
       duration: const Duration(milliseconds: 300),
       width: isExpanded ? 180 : 55,
