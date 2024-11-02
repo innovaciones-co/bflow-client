3b1335346496af7a8ad847572b4f30bda24006cd
saralesan <saralesan@hotmail.com>
2024-11-01 23:37:11 -0500

    refactor: reorganize button bar in BillOfMaterials view

--- lib/src/features/purchase_orders/presentation/widgets/job_materials_widget.dart
+++ lib/src/features/purchase_orders/presentation/widgets/job_materials_widget.dart
@@ -431,7 +431,7 @@ class JobMaterialsWidget extends StatelessWidget with Validator {
 
     if (state is ItemsLoaded) {
       var itemModified = state.selectedItems.isNotEmpty;
-      if (itemModified) {
+      if (itemModified && (context.isMobile || context.isSmallTablet)) {
         homeBloc.add(
           ShowFooterActionEvent(
             showCancelButton: false,
