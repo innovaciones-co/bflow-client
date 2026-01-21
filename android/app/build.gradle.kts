c9afdf4f231b0df42f47f9b6eefc5f67859a77cf
Sergio Torres <sdtorresl@unal.edu.co>
2026-01-21 15:12:59 -0500

    Refactor: Android and macOS build configurations
    - Removed legacy `build.gradle` and `settings.gradle` files in favor of Kotlin DSL equivalents.
    - Updated `gradle-wrapper.properties` to use Gradle version 8.14.
    - Cleaned up `GeneratedPluginRegistrant.swift` by removing unused imports.
    - Updated dependencies in `pubspec.lock` and `pubspec.yaml` to their latest versions, including major updates for `fluttertoast` and `package_info_plus`.
    - Adjusted versioning in `pubspec.yaml` to reflect the new build number.
    - Introduced new `build.gradle.kts` files for both the app and root project to streamline build configurations.

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,63 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+android {
+    namespace = "co.innovaciones.bflow_client"
+    compileSdk = flutter.compileSdkVersion
+    //ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "co.innovaciones.bflow_client"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://docs.flutter.dev/deployment/android#reviewing-the-gradle-build-configuration.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    signingConfigs {
+         create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+dependencies {}
